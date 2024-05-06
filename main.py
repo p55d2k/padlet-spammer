50a968ed38e4b5fd650b869f9a3f20438ed8b64f
p55d2k <231637w@student.hci.edu.sg>
2024-05-06 11:10:27 +0800

    v1.2 added unsafe threading

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
+import threading
 import random
 import json
 import math
 import os
 
 print("\033c")
-print("padlet-spammer by @p55d2k and @InsanelyAvner\n")
-
+print("padlet-spammer v1.2 by @p55d2k and @InsanelyAvner")
+print("NOTE: DO NOT LEAVE THE CHROME WINDOW WHILE THE SCRIPT IS RUNNING\n")
+    
 with open("config.json") as f:
     config = json.load(f)
 
 if not "link" in config or not "comments" in config or not "like" in config:
     raise Exception("Invalid config file")
-
 
 if "amt" in config:
     if type(config["amt"]) != int:
@@ -25,6 +26,15 @@ if "amt" in config:
     amount = config["amt"]
 else:
     amount = math.inf
+    
+if "threads" in config:
+    if type(config["threads"]) != int:
+        raise Exception("Invalid config file")
+    THREADS = config["threads"]
+else:
+    THREADS = 2
+    
+showwindow = True
 
 url = config["link"]
 comment_type = config["comments"]
@@ -51,37 +61,55 @@ elif comment_type == "negative":
 
 chrome_options = Options()
 chrome_options.add_argument("--log-level=3")
-
-driver = webdriver.Chrome(options=chrome_options)
-driver.maximize_window()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+if not showwindow:
+    chrome_options.add_argument("--headless")
 
 count = 1
 
-while amount > 0:
-    print(f"\nIteration {count}")
-    driver.get(url)
+def worker():
+    global count, amount
 
-    wait = WebDriverWait(driver, 10)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
 
-    if like:
-        print("Liking...")
-        like_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[3]/button')))
-        like_button.click()
-    
-    if comment_type == "none":
-        count += 1
+    while amount > 0:
+        print(f"\nIteration {count} on thread {threading.current_thread().name}")
+        driver.get(url)
+
+        wait = WebDriverWait(driver, 10)
+
+        if like:
+            like_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[3]/button')))
+            like_button.click()
+        
+        if comment_type == "none":
+            count += 1
+            amount -= 1
+            driver.delete_all_cookies()
+            continue
+        
+        comment_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[4]/div[2]/div/div/div/div/div/p')))
+        comment_box.send_keys(random.choice(comments))
+        
+        comment_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[4]/div[2]/div/button')))
+        comment_button.click()
+
         amount -= 1
+        count += 1
         driver.delete_all_cookies()
-        continue
-    
-    print("Commenting...")
+        
+threads = []
 
-    comment_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[4]/div[2]/div/div/div/div/div/p')))
-    comment_box.send_keys(random.choice(comments))
+for i in range(THREADS):
+    thread = threading.Thread(target=worker)
+    thread.start()
+    threads.append(thread)
     
-    comment_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="surface-container"]/div[4]/div[2]/div/div[2]/div/div[4]/div[2]/div/button')))
-    comment_button.click()
-
-    amount -= 1
-    count += 1
-    driver.delete_all_cookies()
+for thread in threads:
+    thread.join()
+    
+print("\nDone!")
